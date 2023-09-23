77769c52cf05cced5885427786998adbd421d203
Anmol Verma <anmolverma102002@gmail.com>
2023-09-23 19:44:09 +0530

    feat: :sparkles: added basic homepage

--- front-page.php
+++ front-page.php
@@ -1,6 +1,9 @@
 <?php get_header(); ?> 
+<!-- As this is a front-page, this means it's a homepage also the first page for our website
+ -->
+<div class="container pt-5 pb-5">
+    <h1><?php the_title(); ?></h1>
 
-<div class="container">
 </div>
 
 <?php get_footer(); ?>
